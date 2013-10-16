2e216f5977a2488244d61ca9538443825fe2f7ad
T. J. Tkacik <tkacik@gmail.com>
2013-10-16 10:07:17 -0400

    added build todo functionality without any content formatting

--- TodoViewer.js
+++ TodoViewer.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	$('#check-complete').on('change', showComplete);
 	$('.todo_body').on('click','input', null, editTodo);
 	$('.todo_form').on('submit', uneditTodo);
-	$('.todo_head').on('click', toggleBody);
+	$('#todo_tool').on('click', '.todo_head', null, toggleBody);
 	$('#new-todo').on('click', newTodo);
 	buildList();
 
@@ -38,7 +38,25 @@ var showComplete = function(e) {
 };
 
 var buildList = function(e) {
-	todo("Build List of Todo Items");
+	var toolBody = $("#todo_tool div.toolBody");
+	toolBody.children("div").remove();
+	
+	var A = TodoItem.all;
+	for (var i=0; i<A.length; i++) {
+		var nextTodo = $("<div></div>").addClass("todo hide_body");
+		nextTodo.append($("<div></div>").addClass("todo_head").append($("<span></span>").addClass("todo_title").text(A[i].title)));
+		
+		var todoBody = $("<div></div>").addClass("todo_body");
+		todoBody.append($("<span></span>").addClass("todo_id").text("ID: " + A[i].id));
+		todoBody.append($("<span></span>").addClass("todo_project").text("Project: " + A[i].project));
+		todoBody.append($("<span></span>").addClass("todo_duedate").text("Due: " + A[i].due_date));
+		todoBody.append($("<span></span>").addClass("todo_priority").text("Priority: " + A[i].priority));
+		todoBody.append($("<span></span>").addClass("todo_status").text("status: " + A[i].complete));
+		todoBody.append($("<span></span>").addClass("todo_edit").append('<input type="submit" value="Edit">'));
+		todoBody.append($("<span></span>").addClass("todo_note").text(A[i].note));
+		
+		toolBody.append(nextTodo.append(todoBody));
+	}
 };
 
 var editTodo = function(e) {
