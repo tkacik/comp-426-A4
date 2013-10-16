40279e13577b13a076845750e6f53ce49a05046a
T. J. Tkacik <tkacik@gmail.com>
2013-10-16 14:16:47 -0400

    moved the buildTodo algorithm into a seperate function

--- TodoViewer.js
+++ TodoViewer.js
@@ -45,30 +45,8 @@ var buildList = function(e) {
 	var toolBody = $("#todo_tool div.toolBody");
 	toolBody.children("div").remove();
 	
-	var A = TodoItem.all;
-	for (var i=0; i<A.length; i++) {
-		var nextTodo = $("<div></div>").addClass("todo");
-		nextTodo.append($("<div></div>").addClass("todo_head").append($("<span></span>").addClass("todo_title").text(A[i].title)));
-		
-		var todoBody = $("<div></div>").addClass("todo_body");
-		todoBody.append($("<span></span>").addClass("todo_index").attr("style","display: none").text(i));
-		todoBody.append($("<span></span>").addClass("todo_edit").append('<input type="submit" value="Edit">'));
-		todoBody.append($("<span></span>").addClass("todo_id").append("<strong>ID: </strong>").append(A[i].id));
-		todoBody.append($("<span></span>").addClass("todo_project").append("<strong>Project: </strong>").append(A[i].project));
-		
-		var duedate;
-		if (A[i].due_date == null)
-			duedate = "none";
-		else { duedate = A[i].due_date.toUTCString().substring(0,16);}
-		todoBody.append($("<span></span>").addClass("todo_duedate").append("<strong>Due: </strong>").append(duedate));
-		todoBody.append($("<span></span>").addClass("todo_priority").append("<strong>Priority: </strong>").append(A[i].priority));
-		
-		var complete = "Incomplete";
-		if (A[i].complete) complete = "Complete";
-		todoBody.append($("<span></span>").addClass("todo_status").append("<strong>Status: </strong>").append(complete));
-		todoBody.append($("<span></span>").addClass("todo_note").text(A[i].note));
-		
-		toolBody.append(nextTodo.append(todoBody));
+	for (var i=0; i<TodoItem.all.length; i++) {
+		toolBody.append(buildTodo($("<div></div>").addClass("todo"),i));
 	}
 };
 
@@ -87,6 +65,44 @@ var toggleBody = function(e) {
 };
 
 var newTodo = function(e) {
-	todo("create new todo and add to list");
+	new TodoItem(" ", " ", " ", new Date(), " ", false);
+	$("#todo_tool div.toolBody").prepend(buildForm($("<div></div>").addClass("todo"), TodoItem.all.length-1));
 	e.preventDefault();
 };
+
+var buildTodo = function(thisTodo, i) {
+	var A = TodoItem.all;
+	thisTodo.append($("<div></div>").addClass("todo_head").append($("<span></span>").addClass("todo_title").text(A[i].title)));
+	
+	var todoBody = $("<div></div>").addClass("todo_body");
+	todoBody.append($("<span></span>").addClass("todo_index").attr("style","display: none").text(i));
+	todoBody.append($("<span></span>").addClass("todo_edit").append('<input type="submit" value="Edit">'));
+	todoBody.append($("<span></span>").addClass("todo_id").append("<strong>ID: </strong>").append(A[i].id));
+	todoBody.append($("<span></span>").addClass("todo_project").append("<strong>Project: </strong>").append(A[i].project));
+	
+	var duedate;
+	if (A[i].due_date == null)
+		duedate = "none";
+	else { 
+		duedate = A[i].due_date.toUTCString().substring(0,16);
+		if (!A[i].complete && new Date() > A[i].due_date)
+			thisTodo.addClass("overdue");
+	}
+	todoBody.append($("<span></span>").addClass("todo_duedate").append("<strong>Due: </strong>").append(duedate));
+	todoBody.append($("<span></span>").addClass("todo_priority").append("<strong>Priority: </strong>").append(A[i].priority));
+	
+	var complete = "Incomplete";
+	if (A[i].complete) {
+		complete = "Complete";
+		thisTodo.addClass("complete");
+	}
+	todoBody.append($("<span></span>").addClass("todo_status").append("<strong>Status: </strong>").append(complete));
+	todoBody.append($("<span></span>").addClass("todo_note").text(A[i].note));	
+
+	return thisTodo.append(todoBody);
+}
+
+var buildForm = function(thisTodo, i) {
+	thisTodo.append($("<div></div>").addClass("todo_head").append($("<span></span>").addClass("todo_title").text(TodoItem.all[i].title)));
+	return thisTodo;
+}
